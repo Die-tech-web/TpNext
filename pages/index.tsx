e0c2aec756572d4e156fcc67f5d4f83ec274fd58
Die-tech-web <dieniang32@gmail.com>
2024-05-13 10:28:15 +0000

    tp nest modifier

---
+++ pages/index.tsx
@@ -0,0 +1,24 @@
+/*import link from "next/link";
+export default function Home() {
+  return (
+    <div>
+        <h1>Welcome to my website</h1>
+        <p>My Next Started App!</p>
+        <hr/>
+        <link rel="/products">Products</link>
+        <br/>
+        <link rel="/about">About</link>
+        
+    </div>
+
+  );
+}_*/
+
+import HomePage from "../components/HomePage"
+
+
+const IndexPage = () =>{
+  return <HomePage/>
+}
+
+export default IndexPage
